22fa9d4f9ea95329fdf497da1055929e6df111ef
Miroslav Šturlan <miroslav.sturlan@gmail.com>
2025-10-06 21:53:19 +0200

    feat: add migration for custom function

---
+++ ClearTreasury.GadgetManagement.Api/Data/Migrations/20251006194747_CreateUserFunction.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClearTreasury.GadgetManagement.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                CREATE FUNCTION dbo.fn_Generate3grams(@input NVARCHAR(MAX))
+                RETURNS NVARCHAR(MAX)
+                WITH SCHEMABINDING
+                AS
+                BEGIN
+                    DECLARE @i INT = 1;
+                	DECLARE @n INT = 3;
+                	DECLARE @len INT = LEN(@input);
+                	DECLARE @result NVARCHAR(MAX) = '';
+
+                    WHILE @i <= @len - @n + 1
+                    BEGIN
+                        SET @result = @result + SUBSTRING(@input, @i, @n) + ' ';
+                        SET @i = @i + 1;
+                    END
+                    RETURN TRIM(@result);
+                END
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                DROP FUNCTION dbo.fn_Generate3grams
+                """);
+        }
+    }
+}
