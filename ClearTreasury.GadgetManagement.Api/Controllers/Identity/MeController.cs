4f6befb6cdb8857facc72925691b43a9be8f50cb
Miroslav Šturlan <miroslav.sturlan@gmail.com>
2025-10-06 23:59:20 +0200

    feat: add helper 'me' endpoint

---
+++ ClearTreasury.GadgetManagement.Api/Controllers/Identity/MeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClearTreasury.GadgetManagement.Api.Controllers.Identity;
+
+public class MeController : AppBaseController
+{
+    [HttpGet]
+    [Authorize]
+    public ActionResult Get()
+    {
+        return Ok(User.Claims.ToDictionary(x => x.Type, x => x.Value));
+    }
+}
