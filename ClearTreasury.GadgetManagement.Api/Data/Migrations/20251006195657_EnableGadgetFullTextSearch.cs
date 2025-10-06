b63f79e7a8d3fcc27cf0e84028331f5d2761a894
Miroslav Šturlan <miroslav.sturlan@gmail.com>
2025-10-06 22:01:31 +0200

    feat: enable gadget full text search

---
+++ ClearTreasury.GadgetManagement.Api/Data/Migrations/20251006195657_EnableGadgetFullTextSearch.cs
@@ -0,0 +1,39 @@
+﻿using ClearTreasury.GadgetManagement.Api.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClearTreasury.GadgetManagement.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnableGadgetFullTextSearch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"""
+                CREATE FULLTEXT CATALOG ftCatalog AS DEFAULT;
+
+                CREATE FULLTEXT INDEX ON {DbSchemas.Inventory}.{nameof(Gadget)}
+                (
+                    {nameof(Gadget.NameGrams)} LANGUAGE 1033
+                )
+                KEY INDEX PK_{nameof(Gadget)}
+                ON ftCatalog
+                WITH CHANGE_TRACKING AUTO;
+                """,
+                suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"""
+                DROP FULLTEXT INDEX ON {DbSchemas.Inventory}.{nameof(Gadget)};
+
+                DROP FULLTEXT CATALOG ftCatalog;
+                """,
+                suppressTransaction: true);
+        }
+    }
+}
